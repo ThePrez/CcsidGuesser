33b15df31416ddc74fa8c0871150a58a51c50f08
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2022-02-20 21:23:06 -0600

    nit: missing paren in help text

--- src/main/java/com/github/theprez/ccsidguesser/CcsidGuesser.java
+++ src/main/java/com/github/theprez/ccsidguesser/CcsidGuesser.java
@@ -239,7 +239,7 @@ public class CcsidGuesser {
         final String usage = "Usage: java -jar ccsidguesser.jar  [options] <file>\n"
                                 + "\n"
                                 + "    Valid options include:\n"
-                                + "        --show=top/topN/all: how many CCSID guesses to show (default is 'top1', which shows the\n"
+                                + "        --show=top/topN/all: how many CCSID guesses to show (default is 'top1'), which shows the\n"
                                 + "                             top 1 result. A value of 'top' shows the top guess and some number\n"
                                 + "                             of very-near guesses\n"
                                 + "        --format=<format>:   output format (default is 'ccsid')\n"
